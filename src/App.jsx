f374f0898a0f2caf9efb655e00da0fb1c69ec381
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-18 06:48:45 +0600

    create a tic-tac-toe branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,137 @@
-function App() {
-  return <></>;
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+function Square({ value, onSquareClick }) {
+  return (
+    <button
+      className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+// eslint-disable-next-line react/prop-types
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    // eslint-disable-next-line react/prop-types
+    const nextSquares = squares.slice();
+    // if (squares[i]) {
+    //   return;
+    // }
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div>{status}</div>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
 
-export default App;
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  // eslint-disable-next-line no-undef
+  const currentSquares = history[currentMove];
+
+  // eslint-disable-next-line no-undef
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpto(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      // eslint-disable-next-line react/jsx-key
+      <li key={move}>
+        <button
+          className="border border-gray-300 p-1 bg-gray-100"
+          onClick={() => jumpto(move)}
+        >
+          {description}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="flex justify-center p-4 gap-5">
+      <div>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
