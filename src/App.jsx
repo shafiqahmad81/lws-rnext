1cdfce7bc6f726d91513638d17a2cea81c543228
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-19 08:07:21 +0600

    2.1 close

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+// eslint-disable-next-line react/prop-types
+function AlertButton({ message, children }) {
+  return <button onClick={() => alert(message)}>{children}</button>;
+}
 function App() {
-  return <></>;
+  return (
+    <>
+      <AlertButton message="Playing!">Play Movie</AlertButton>
+      <AlertButton message="Uploading!">Upload Image</AlertButton>
+    </>
+  );
 }
 
 export default App;
