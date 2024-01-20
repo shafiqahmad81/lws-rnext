77180736e57e3952aa6074450bfa4e9c8801d3f0
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-20 23:42:45 +0600

    update 2.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          console.dir(e);
+          e.preventDefault();
+        }}
+      >
+        <input className=" bg-gray-300" />
+        <button>Send</button>
+      </form>
+    </>
+  );
 }
-
-export default App;
