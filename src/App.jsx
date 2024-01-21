122a589f89b624b3f855ba641c4d91f2eb1de01f
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-21 22:07:28 +0600

    learn useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import { sculpturelist } from "./data.jsx";
 function App() {
-  return <></>;
+  const [index, setIndex] = useState(0);
+  const [showmore, setShowmore] = useState(false);
+  function handleClick() {
+    setIndex(index + 1);
+  }
+  function handlemoreClick() {
+    setShowmore(!showmore);
+  }
+  let sculpture = sculpturelist[index];
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name}</i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculpturelist.length})
+      </h3>
+      <button onClick={handlemoreClick}>
+        {showmore ? "Hide" : "Show"} details
+      </button>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
+  );
 }
 
 export default App;
