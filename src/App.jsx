30d649f26fd497aec3d4383a70b20bb245a4463b
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-28 23:15:25 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Profile } from "./components";
+
 function App() {
-  return <> </>;
+  return (
+    <div className="ml-5 mt-5">
+      <h1 className="text-2xl font-bold text-center">Amazing scientists</h1>
+      <div className="flex gap-1 justify-center mt-2">
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+    </div>
+  );
 }
 
 export default App;
