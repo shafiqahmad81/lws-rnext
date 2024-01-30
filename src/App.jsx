0b3aa40ede4c6dc7e2567f31c5e40f6c3411189f
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-30 07:49:43 +0600

    complete 1.7 branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-function App() {
-  return <></>;
-}
+import Profile from "./profile";
 
-export default App;
+const person = {
+  name: "Shafiq",
+
+  them: {
+    fontSize: "50px",
+    backgroundColor: "red",
+  },
+};
+
+const style1 = {
+  backgroundColor: "red",
+};
+
+const style2 = {
+  backgroundColor: "green",
+};
+
+const color = "black";
+
+export default function App() {
+  return (
+    <div style={color === "green" ? style1 : style2}>
+      <h1>{person.name} This is my templeate</h1>
+      <Profile />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, vero.
+      </p>
+    </div>
+  );
+}
---
+++ src/profile.jsx
@@ -0,0 +1,14 @@
+const man = {
+  width: "200px",
+};
+export default function Profile() {
+  return (
+    <>
+      <img
+        style={man}
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+    </>
+  );
+}
