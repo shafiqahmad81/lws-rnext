f53560540447050cd84e88c2215424e88ee5bea0
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-02-06 14:52:21 +0600

    create profile update

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,9 +1,3 @@
-export default function Profile({ person, size }) {
-  return (
-    <div>
-      <h1 style={{ fontSize: "50px", color: "green" }}>{person.name}</h1>
-      <p>{person.imageId}</p>
-      <div>{size}</div>
-    </div>
-  );
+export default function Profile({ children }) {
+  return <div>{children}</div>;
 }
