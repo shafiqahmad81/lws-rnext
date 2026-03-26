a09a8bf7fcec0200994e3a4d223212051a88859e
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-03-26 16:49:42 +0600

    update new

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
-    <div>
+    <>
       <h1 className="text-5xl text-center font-bold">This is a new page</h1> 
-    </div>
+    </>
   );
 }
  
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <div className="container py-2">
+      <App />
+    </div>
   </React.StrictMode>
 );
